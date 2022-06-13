62dc2ce4627d052ad1ee6ee6e5852de29f82d1ea
Winnicius-Moura <winnicius.moura@gmail.com>
2022-06-13 18:33:13 -0300

    adicionando arquivos

---
+++ main.js
@@ -0,0 +1,35 @@
+window.addEventListener('scroll', onScroll);
+
+onScroll();
+
+function onScroll(){
+    showBackToTopButtonOnScroll()
+    showNavOnScroll()
+
+}
+
+function showNavOnScroll(){
+    if(scrollY > 0){
+        navigation.classList.add('scroll');
+    } else{
+        navigation.classList.remove('scroll');
+    }
+}
+
+
+
+function showBackToTopButtonOnScroll(){
+    if(scrollY > 500){
+        navigation.classList.add('show');
+    } else{
+        navigation.classList.remove('show');
+    }
+}
+
+function openMenu(){
+    document.body.classList.add('menu-expanded');
+}
+
+function closeMenu(){
+    document.body.classList.remove('menu-expanded');
+}
