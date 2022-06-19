fbac803f0e9fe997ed17021c241ad262f1dd01a3
Winnicius-Moura <winnicius.moura@gmail.com>
2022-06-19 15:24:13 -0300

    add section duvidas

--- main.js
+++ main.js
@@ -33,3 +33,21 @@ function openMenu(){
 function closeMenu(){
     document.body.classList.remove('menu-expanded');
 }
+
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++){
+    acc[i].addEventListener("click", function(){
+
+        this.classList.toggle("active");
+    
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block"){
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
